9010e09c71835a7ee67e24a3dc18ab6650023714
uzadow <uzadow@libavg.de>
2006-12-24 14:52:52 +0000

    Added camera & tracker params to calibrator, new keyboard interface.
    svn path=/trunk/mtc/; revision=1827

--- Calibrator.py
+++ Calibrator.py
@@ -3,110 +3,158 @@
 
 import avg
 
-import unittest
-import sys, syslog
+import sys, os, math, stat
+import avg
+import anim
 
-global ourTrackingTestCase
+global gTracker
+global gCalibrator
+
+class Calibrator:
+    global gPlayer
+    def __init__(self, Tracker):
+        self.__isActive = False
+        self.__Tracker = Tracker
+        self.__ParamList = [
+            {'Name':"threshold", 'min':1, 'max':255, 'increment':1},
+            {'Name':"brightness", 'min':1, 'max':255, 'increment':1},
+            {'Name':"exposure", 'min':7, 'max':62, 'increment':1},
+            {'Name':"shutter", 'min':1, 'max':533, 'increment':1},
+            {'Name':"gain", 'min':16, 'max':64, 'increment':1},
+            {'Name':"trapezoid", 'min':0, 'max':1, 'increment':0.01}
+        ]
+        self.__curParam = 0
+    def __updateBitmap(self, ImgName, ID):
+        Bitmap = self.__Tracker.getImage(ID)
+        Node = gPlayer.getElementByID(ImgName)
+        Node.setBitmap(Bitmap)
+        if ImgName != "fingers":
+            Node.width=Bitmap.getSize()[0]/4
+            Node.height=Bitmap.getSize()[1]/4
+        else:
+            Node.width=1280
+            Node.height=960
+    def __getParam(self, Name):
+        if Name == "threshold":
+            return gTracker.threshold
+        elif Name == "brightness":
+            return gTracker.brightness
+        elif Name == "exposure":
+            return gTracker.exposure
+        elif Name == "shutter":
+            return gTracker.shutter
+        elif Name == "gain":
+            return gTracker.gain
+        elif Name == "trapezoid":
+            return gTracker.trapezoid
+        else:
+            print "getParam broken"
+    def __setParam(self, Name, Val):
+        if Name == "threshold":
+            gTracker.threshold = Val
+        elif Name == "brightness":
+            gTracker.brightness = Val
+        elif Name == "exposure":
+            gTracker.exposure = Val
+        elif Name == "shutter":
+            gTracker.shutter = Val
+        elif Name == "gain":
+            gTracker.gain = Val
+        elif Name == "trapezoid":
+            gTracker.trapezoid = Val
+        else:
+            print "setParam broken"
+    def __displayParams(self):
+        i = 0
+        for Param in self.__ParamList:
+            Node = gPlayer.getElementByID("param"+str(i))
+            Name = Param['Name']
+            Val = self.__getParam(Name)
+            Node.text = Param['Name']+": "+str(Val)
+            if self.__curParam == i:
+                Node.color = "FFFFFF"
+            else:
+                Node.color = "A0A0FF"
+            i += 1
+    def __changeParam(self, Change):
+        curParam = self.__ParamList[self.__curParam]
+        Val = self.__getParam(curParam['Name'])
+        Val += Change*curParam['increment']
+        if Val < curParam['min']:
+            Val = curParam['min']
+        if Val > curParam['max']:
+            Val = curParam['max']
+        self.__setParam(curParam['Name'], Val)
+    def onFrame(self):
+        self.__updateBitmap("camera", avg.IMG_CAMERA);
+        self.__updateBitmap("nohistory", avg.IMG_NOHISTORY);
+        self.__updateBitmap("histogram", avg.IMG_HISTOGRAM);
+        self.__updateBitmap("fingers", avg.IMG_FINGERS);
+    def switchActive(self):
+        if self.__isActive:
+            self.__isActive = False
+            gPlayer.getElementByID("fingers").opacity = 0
+            gPlayer.getElementByID("tracking").opacity = 0
+            gPlayer.clearInterval(self.__onFrameID)
+        else:
+            self.__isActive = True
+            gPlayer.getElementByID("fingers").opacity = 1 
+            gPlayer.getElementByID("tracking").opacity = 1
+            self.__onFrameID = gPlayer.setInterval(1, self.onFrame)
+            self.__displayParams()
+        self.__Tracker.debug = self.__isActive
+    def onKeyUp(self, Event):
+        if Event.keystring == "up":
+            if self.__curParam > 0:
+                self.__curParam -= 1
+        elif Event.keystring == "down":
+            if self.__curParam < len(self.__ParamList)-1:
+                self.__curParam += 1
+        elif Event.keystring == "left":
+            self.__changeParam(-1)
+        elif Event.keystring == "right":
+            self.__changeParam(1)
+        elif Event.keystring == "page up":
+            self.__changeParam(-10)
+        elif Event.keystring == "page down":
+            self.__changeParam(10)
+        else:
+            print "Unknown key ", Event.keystring
+        self.__displayParams()
+    def isActive(self):
+        return self.__isActive
 
 def onKeyUp():
-    Event = Player.getCurEvent()
-    ourTrackingTestCase.onKeyUp(Event)
+    global gCalibrator
+    global gPlayer
+    Event = gPlayer.getCurEvent()
+    if Event.keystring == "t":
+        gCalibrator.switchActive()
+    elif gCalibrator.isActive():
+        gCalibrator.onKeyUp(Event)
 
-class TrackingTestCase(unittest.TestCase):
-    def __init__(self, testFuncName):
-        self.__testFuncName = testFuncName
-        unittest.TestCase.__init__(self, testFuncName)
-        print "-------- ", self.__testFuncName, " --------"
-        global ourTrackingTestCase
-        ourTrackingTestCase = self
-    def onKeyUp(self, Event):
-        if Event.keystring == "a":
-            self.__tracker.threshold += 1
-            print "Threshold: ", self.__tracker.threshold
-        elif Event.keystring == "y":
-            self.__tracker.threshold -= 1
-            print "Threshold: ", self.__tracker.threshold
-        elif Event.keystring == "s":
-            self.__tracker.brightness += 5
-            print "Brightness: ", self.__tracker.brightness
-        elif Event.keystring == "x":
-            self.__tracker.brightness -= 5
-            print "Brightness: ", self.__tracker.brightness
-        elif Event.keystring == "d":
-            self.__tracker.exposure += 1
-            print "Exposure: ", self.__tracker.exposure
-        elif Event.keystring == "c":
-            self.__tracker.exposure -= 1
-            print "Exposure: ", self.__tracker.exposure
-        elif Event.keystring == "f":
-            self.__tracker.shutter += 5
-            print "Shutter: ", self.__tracker.shutter
-        elif Event.keystring == "v":
-            self.__tracker.shutter -= 5
-            print "Shutter: ", self.__tracker.shutter
-        elif Event.keystring == "g":
-            self.__tracker.gain += 5
-            print "Gain: ", self.__tracker.gain
-        elif Event.keystring == "b":
-            self.__tracker.gain -= 5
-            print "Gain: ", self.__tracker.gain
-        elif Event.keystring == "j":
-            self.__tracker.trapezoid -= 0.01
-            print "Trapezoid: ", self.__tracker.trapezoid
-        elif Event.keystring == "m":
-            self.__tracker.trapezoid += 0.01
-            print "Trapezoid: ", self.__tracker.trapezoid
-        elif Event.keystring == "h":
-            self.__tracker.resetHistory()
-            print "History reset"
-        elif Event.keystring == "w":
-            self.__tracker.saveConfig()
-            print ("Tracker configuration saved.")
-        elif Event.keystring == "e":
-            self.__saveIndex += 1
-            self.__tracker.getImage(avg.IMG_NOHISTORY).save("img"+str(self.__saveIndex)+".png")
-            print ("Image saved.")
-        elif Event.keystring == "r":
-            self.__tracker.debug = not(self.__tracker.debug)
-            print "Debug image generation: ", self.__tracker.debug
-    def updateBitmap(self, ImgName, ID):
-        Bitmap = self.__tracker.getImage(ID)
-        Node = Player.getElementByID(ImgName)
-        Node.setBitmap(Bitmap)
-        Node.width=Bitmap.getSize()[0]/4
-        Node.height=Bitmap.getSize()[1]/4
-    def onFrame(self):
-        self.updateBitmap("camera", avg.IMG_CAMERA);
-        self.updateBitmap("nohistory", avg.IMG_NOHISTORY);
-        self.updateBitmap("histogram", avg.IMG_HISTOGRAM);
-#        self.updateBitmap("highpass", avg.IMG_HIGHPASS);
-        self.updateBitmap("fingers", avg.IMG_FINGERS);
-    def test(self):
-        Player.loadFile("calibrator.avg")
-        Player.setFramerate(60)
-        self.__tracker = Player.addTracker("/dev/video1394/0", 30, "640x480_MONO8")
-        Player.setInterval(1, self.onFrame)
-#        Player.setResolution(0, 1280, 720, 24)
-        self.__saveIndex = 0
-        Player.play()
-
-def playerTestSuite():
-    suite = unittest.TestSuite()
-    suite.addTest(TrackingTestCase("test"))
-    return suite
-
-Player = avg.Player()
+gPlayer = avg.Player()
 Log = avg.Logger.get()
+bDebug = not(os.getenv('AVG_DEPLOY'))
+if (bDebug):
+    gPlayer.setResolution(0, 0, 0, 0) 
+else:
+    gPlayer.setResolution(1, 0, 0, 0)
+#    Log.setFileDest("/var/log/cleuse.log")
 Log.setCategories(Log.APP |
-          Log.WARNING | 
-          Log.PROFILE |
-#          Log.PROFILE_LATEFRAMES |
-          Log.CONFIG |
-          Log.MEMORY
-#          Log.EVENTS2 |
-#          Log.BLTS    |
-#          Log.EVENTS
-          )
+                  Log.WARNING | 
+                  Log.PROFILE |
+#                 Log.PROFILE_LATEFRAMES |
+                  Log.CONFIG  
+#                 Log.MEMORY  |
+#                 Log.BLTS    
+#                  Log.EVENTS |
+#                  Log.EVENTS2
+                 )
+gPlayer.loadFile("calibrator.avg")
+anim.init(gPlayer)
+gPlayer.setVBlankFramerate(1)
+gTracker = gPlayer.addTracker("/dev/video1394/0", 30, "640x480_MONO8")
+gCalibrator = Calibrator(gTracker)
+gPlayer.play()
 
-runner = unittest.TextTestRunner()
-runner.run(playerTestSuite())
